0932f6dd2001aa23ccf3c0c4113d65db1ae5c163
VerdiGit <estefania.lop.ru@gmail.com>
2022-08-10 23:53:11 -0500

    rango const

--- Frasesrandom.js
+++ Frasesrandom.js
@@ -2,3 +2,9 @@ const frutas=['manzana', 'pera', 'uva'];
 const proteinas=['pollo', 'pescado', 'res'];
 const ejercicio=['correr', 'caminar', 'nadar'];
 
+//prints the three lines of a random 
+console.log(futas[getRandomIndex(getMaxNumber(frutas))]);
+console.log(proteinas[getRandomIndex(getMaxNumber(proteinas))]);
+console.log(ejercicio[getRandomIndex(getMaxNumber(ejercicio))]);
+
+let rango= frutas.length;
